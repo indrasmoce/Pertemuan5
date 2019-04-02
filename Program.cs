ed41d767bcd80032c730831b393d60ba59d5b8bf
Indra Bagus Setiawan <48389065+indrasmoce@users.noreply.github.com>
2019-04-02 14:40:15 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indra
+{
+    class Calculator
+    {
+        public int Penjumlahan(int a, int b)
+        {
+            return a+b;
+        }
+        public int Penjumlahan(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        public int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        public int Pekalian(int a, int b)
+        {
+            return a * b;
+        }
+        
+    }
+}

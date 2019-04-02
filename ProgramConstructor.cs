1f30fc83ea8fc083ad1662c37302fe4988eb95b9
Indra Bagus Setiawan <48389065+indrasmoce@users.noreply.github.com>
2019-04-02 15:04:37 +0700

    Add files via upload

---
+++ ProgramConstructor.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P5
+{
+  public class Mahasiswa
+    {
+        public Mahasiswa()
+        { }
+        public Mahasiswa(string nim, string nama)
+        {
+            Nim = nim;
+            Nama = nama;
+        }
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        static void Main(string[] args)
+        {
+            Mahasiswa mhs1 = new Mahasiswa();
+            CetakHasil(mhs1);
+            Console.WriteLine();
+
+            Mahasiswa mhs2 = new Mahasiswa("12345", "Paijo");
+            CetakHasil(mhs2);
+            Console.ReadKey();
+        }
+        static void CetakHasil(Mahasiswa mhs)
+        {
+            Console.WriteLine("Nim : {0}", mhs.Nim);
+            Console.WriteLine("Nama : {0}",mhs.Nama);
+        }
+    }
+}

833cbd1666e15f97acda6ac6801ff157efd4fcf0
indrasmoce <indra.2268@students.amikom.ac.id>
2019-04-02 14:36:45 +0700

    update program pertemuan 5

---
+++ proses.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indra
+{
+    class proses
+    {
+
+
+        static void Main(string[] args)
+        {  
+            int hasil;
+            Calculator calculator =new  Calculator();
+            hasil = calculator.Penjumlahan(10, 2);
+            CetakHasil("Hasil Penjumlahan : " + hasil);
+
+            hasil = calculator.Penjumlahan( 10, 2 , 3);
+            CetakHasil("Hasil Penjumlahan Overload  : " + hasil);
+
+            hasil = calculator.Pengurangan(7, 2);
+            CetakHasil("Hasil Pengurangan  : " + hasil);
+
+            hasil = calculator.Pekalian(5, 2);
+            CetakHasil("Hasil Perkalian : " + hasil);
+
+            Console.ReadKey();
+        }
+        static void CetakHasil(string hasil)
+        { Console.WriteLine(hasil); }
+    }
+}
